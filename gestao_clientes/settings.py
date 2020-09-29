c83c65649cd984c5874e7e99aec4854fe47a82dd
GABRIEL LIMA NUNES <gabriel_lima_nunes@hotmail.com>
2020-09-28 22:02:51 -0300

    some comment

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+# hosts list
 ALLOWED_HOSTS = ['gestao-clientes-gabriel.herokuapp.com']
 
 
